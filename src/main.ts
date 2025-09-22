8ffbb130b4808c3353c6f82d8454abd79320fa59
Leonardo Krindges <krindgesleonardo3@gmail.com>
2025-09-22 20:13:15 -0300

    feat: add Swagger configuration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const config = {
+	envMode: process.env.MODE_ENV || 'development',
+};
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	if (config.envMode === 'development') {
+		const swaggerConfig = new DocumentBuilder()
+			.setTitle('Edu Class API')
+			.setDescription('The Edu Class API description')
+			.setVersion('1.0')
+			.build();
+
+		const document = SwaggerModule.createDocument(app, swaggerConfig);
+		SwaggerModule.setup('api', app, document);
+	}
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
