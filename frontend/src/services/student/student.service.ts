62e788861ca9136c32945f5fcfd2a7f2103ec0d7
Leonardo Krindges <krindgesleonardo3@gmail.com>
2025-10-10 15:36:53 -0300

    feat(student): implement FindAllQueryParamsDto and update findAll method to support query parameters

--- frontend/src/services/student/student.service.ts
+++ frontend/src/services/student/student.service.ts
@@ -1,6 +1,7 @@
-import { http } from "@/http-module/http-client";
+import { http, PaginatedResponse } from "@/http-module/http-client";
 import { Student } from "@/interfaces/student/student.interface";
 import { StudentDTO } from "./dtos/student.dto";
+import { FindAllQueryParamsDto } from "./dtos/find-all-student-query.dto";
 
 class StudentService {
   private readonly baseUrl = "/students";
@@ -29,20 +30,22 @@ class StudentService {
     return response.body;
   }
 
-  //   async findAll(params?: {
-  //     page?: number;
-  //     limit?: number;
-  //     search?: string;
-  //     status?: string;
-  //   }): Promise<{
-  //     data: Student[];
-  //     total: number;
-  //     page: number;
-  //     limit: number;
-  //   }> {
-  //     const response = await http.get(this.baseUrl, { params });
-  //     return response.body;
-  //   }
+  async findAll(
+    params?: FindAllQueryParamsDto
+  ): Promise<PaginatedResponse<Student>> {
+    const queryParams = new URLSearchParams();
+    if (params?.page) queryParams.append("page", params.page.toString());
+    if (params?.limit) queryParams.append("limit", params.limit.toString());
+    if (params?.search) queryParams.append("search", params.search);
+    if (params?.status) queryParams.append("status", params.status);
+    const response = await http.get<PaginatedResponse<Student>>(
+      `${this.baseUrl}?${queryParams.toString()}`
+    );
+    if (response.status !== 200) {
+      throw new Error("Falha ao buscar estudantes, tente novamente.");
+    }
+    return response.body;
+  }
 
   async delete(id: string): Promise<void> {
     await http.delete(`${this.baseUrl}/${id}`);
